ecf682946a82e7ec552b968fd6224f334f38bdff
engrnazrulislam <nazrul.cmt12@gmail.com>
2024-05-09 19:50:33 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-green-700 font-bold'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
